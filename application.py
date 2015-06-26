7d804713f07179088fdce50e0e1ecc7ec96d0e7e
AngelinaRS <candy26y_@hotmail.com>
2015-06-26 09:15:03 -0600

    Number to High

--- application.py
+++ application.py
@@ -1,4 +1,9 @@
 import random #This module will generate a random number
 
-RANDOM_NUMBER = random.randint(1,20) #This variable saves the number generated
+RANDOM_NUMBER = random.randint(1,20) #This variable saves the number generated from 1 to 20
 print RANDOM_NUMBER
+
+PROPOSAL_OF_THE_USER = int(input("Enter a number from 1 to 20")) #This variable saves the proposal of the user
+
+if PROPOSAL_OF_THE_USER > RANDOM_NUMBER: #This condition compares if the proposal of the user is higher than the number generated
+	print "You guessed to high, please try again"
