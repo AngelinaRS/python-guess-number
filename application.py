2d59e9c64ac943ead1c5068e97d4061d9b1b6418
AngelinaRS <candy26y_@hotmail.com>
2015-06-26 09:45:26 -0600

    Number too low

--- application.py
+++ application.py
@@ -1,9 +1,12 @@
 import random #This module will generate a random number
 
-RANDOM_NUMBER = random.randint(1,20) #This variable saves the number generated from 1 to 20
+RANDOM_NUMBER = random.randint(1,20) #This variable saves the generated number from 1 to 20
 print RANDOM_NUMBER
 
 PROPOSAL_OF_THE_USER = int(input("Enter a number from 1 to 20")) #This variable saves the proposal of the user
 
-if PROPOSAL_OF_THE_USER > RANDOM_NUMBER: #This condition compares if the proposal of the user is higher than the number generated
-	print "You guessed to high, please try again"
+if PROPOSAL_OF_THE_USER > RANDOM_NUMBER: #This condition compares if the proposal of the user is higher than the generated number
+	print "You guessed too high, please try again"
+
+elif PROPOSAL_OF_THE_USER < RANDOM_NUMBER: #This condition compares if the proposal of the user is lower than the generated number
+	print "You guessed too low, please try again" 
